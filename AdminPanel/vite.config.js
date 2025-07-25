993ea056c2e6c805738f058d6813e5bbb094d91c
pops <pops@DESKTOP-P3QPI08>
2025-07-25 18:02:46 +0300

    added port

--- AdminPanel/vite.config.js
+++ AdminPanel/vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; 
+import path from 'path';
 
 export default defineConfig({
+  server: {
+    port: process.env.PORT || 4173,  // Порт по умолчанию для preview, если не указано в окружении
+    host: '0.0.0.0',  // Делаем сервер доступным для внешних подключений
+  },
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@config': path.resolve(__dirname, './src/config'), 
+      '@config': path.resolve(__dirname, './src/config'),
     },
   },
 });
